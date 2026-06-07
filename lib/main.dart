97c43fa88fba465c6016894f7036a3e09421e461
florsoru <soruflorelena@gmail.com>
2026-06-06 23:07:15 -0600

    versión final

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,9 @@ class MyApp extends StatelessWidget {
       title: 'Board Scan',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // Aquí generamos la paleta completa basada en tu color #6B0000
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color(0x7EA67B),
-          brightness: Brightness
-              .light, // puedes cambiar a dark si prefieres modo oscuro
+          brightness: Brightness.light,
         ),
         useMaterial3: true,
       ),
